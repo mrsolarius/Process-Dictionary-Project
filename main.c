5ad9bbb115e65d564a2923792ee5ad7bd0a91d42
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-10-08 12:40:46 +0200

    Création de plusieur processus

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-int main(){
-
-	printf("hello word\n");
-	return 0;
+int main()
+{
+    for(int i=0;i<5;i++) // Generer 5 fis
+    {
+        if(fork() == 0)
+        {
+            printf("[fis] pid %d [père] pid %d\n",getpid(),getppid());
+            exit(0);
+        }
+    }
+    for(int i=0;i<5;i++) // attendre la génération des fis
+    wait(NULL);
+    return 0;
 }
