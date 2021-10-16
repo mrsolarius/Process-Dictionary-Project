e2e9f08eab417f1d42106686fb0c05b51a750292
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-10-16 21:23:38 +0200

    Doc : comentaire des tests unitaire

--- process_dictionary/testDDP.c
+++ process_dictionary/testDDP.c
@@ -46,7 +46,7 @@ void passTest(char *testFunction, char *msg, bool res) {
 }
 
 void resume() {
-    printf(COLOR_YELLOW"\n%d teste réussie sur %d (%d/%d)"COLOR_RESET, testPassed, totalTest, testPassed, totalTest);
+    printf(COLOR_YELLOW"\n%d teste réussie sur %d (%d/%d)\n"COLOR_RESET, testPassed, totalTest, testPassed, totalTest);
 }
 
 /*****************************
@@ -55,16 +55,19 @@ void resume() {
 bool evaluateType_itShouldThrowsENOTDDPError() {
     DDP_Errno = -1;
     char val = evaluateType(0xff);
+    //La commande 0xff n'existe pas dans le protocole DDP cela doit donc renvoyer l'erreur ENOTDDP
     return (DDP_Errno == ENOTDDP) && (val == -1);
 }
 
 bool evaluateType_itShouldBeAskFrame() {
     char frame = evaluateType(C_SET);
+    //La commande C_SET est de type ASK la fonction doit donc renvoyer ASK(0x1)
     return frame == ASK;
 }
 
 bool evaluateType_itShouldBeAcquittalFrame() {
     char frame = evaluateType(A_SET);
+    //La commande A_SET est de type ACQUITTAL la fonction doit donc renvoyer ACQUITTAL(0x2)
     return frame == ACQUITTAL;
 }
 
@@ -75,6 +78,8 @@ bool decodeAskFrame_itShouldThrowsENOTDDPError() {
     DDP_Errno = -1;
     unsigned char frame[] = {C_SET, 'n', 'o', 't', ' ', 'a', ' ', 'f', 'r', 'a', 'm'};
     PAskFrame res = decodeAskFrame(frame);
+    //La trame envoyer commence par une commande mais la suite n'existe pas donc ce n'est pas une trame du
+    //protocole DDP on renvoie l'erreur ENOTDDP
     return (DDP_Errno == ENOTDDP) && (res->cmd == 0xff);
 }
 
@@ -82,6 +87,8 @@ bool decodeAskFrame_itShouldThrowENOTASK() {
     DDP_Errno = -1;
     unsigned char frame[] = {A_SET, 2, SUCCESS, END_FRAME};
     PAskFrame res = decodeAskFrame(frame);
+    //La trame envoyer et correct cependant la commande A_SET n'est pas une commande de type ASK
+    //il faut donc renvoyer l'erreur ENOTASK
     return (DDP_Errno == ENOTASK) && (res->cmd == 0xff);
 }
 
@@ -89,6 +96,8 @@ bool decodeAskFrame_itShouldThrowEBADCMD() {
     DDP_Errno = -1;
     unsigned char frame[] = {C_SET, END_FRAME};
     PAskFrame res = decodeAskFrame(frame);
+    //La commande C_SET doit obligatoirement avoir une valeur donc on renvoie une erreur EBADCMD
+    //Car cela devrais plutôt être la commande C_EXIT qui devrais être envoyer
     return (DDP_Errno == EBADCMD) && (res->cmd == 0xff);
 }
 
@@ -96,6 +105,8 @@ bool decodeAskFrame_itShouldThrowENOENDFLAG() {
     DDP_Errno = -1;
     unsigned char frame[] = {C_EXIT, 2};
     PAskFrame res = decodeAskFrame(frame);
+    //Ici la derniere valeur du tableau devrais être END_FRAME or ici ce n'est que 2
+    //On s'attend donc à recevoir l'erreur NO END FLAG
     return (DDP_Errno == ENOENDFLAG) && (res->cmd == 0xff);
 }
 
@@ -103,6 +114,8 @@ bool decodeAskFrame_itShouldThrowEBADCMD_2() {
     DDP_Errno = -1;
     unsigned char frame[] = {C_EXIT, 0xA, 0xA, END_FRAME};
     PAskFrame res = decodeAskFrame(frame);
+    //La commande EXIT ne peut pas avoir de valeur donc on renvoie une erreur EBADCMD
+    //Car cela devrais plutôt être les commandes C_SET C_LOOKUP ou C_DUMP qui devrais être envoyer
     return (DDP_Errno == EBADCMD) && (res->cmd == 0xff);
 }
 
@@ -110,28 +123,26 @@ bool decodeAskFrame_itShouldThrowENOENDFLAG_2() {
     DDP_Errno = -1;
     unsigned char frame[] = {C_SET, 0xA, 0xA, 0xFF};
     PAskFrame res = decodeAskFrame(frame);
+    //Ici la derniere valeur du tableau devrais être END_FRAME or ici ce n'est que 2
+    //On s'attend donc à recevoir l'erreur NO END FLAG
     return (DDP_Errno == ENOENDFLAG) && (res->cmd == 0xff);
 }
 
 bool decodeAskFrame_itShouldCorrectlyDecodeThisFrame() {
     DDP_Errno = -1;
-    /**
-     * On veut envoyer la valeur 125 soit 0b0000 0000 0111 1101
-     * on envoie donc 0 0x7D
-     */
+     //On veut envoyer la valeur 125 soit 0b0000 0000 0111 1101
+     //on envoie donc 0 0x7D
     unsigned char frame[] = {C_SET, 0 + 1, 0x7D + 1, END_FRAME};
     PAskFrame res = decodeAskFrame(frame);
+    //Ici la frame et bien former et devrais donc renvoyer les bonnes valeurs
     return (res->cmd == C_SET) && (res->val == 125);
 }
 
 bool decodeAskFrame_itShouldCorrectlyDecodeThisFrame_2() {
     DDP_Errno = -1;
-    /**
-     * On veut envoyer la valeur 125 soit 0b0000 0000 0111 1101
-     * on envoie donc 0 0x7D
-     */
     unsigned char frame[] = {C_EXIT, END_FRAME};
     PAskFrame res = decodeAskFrame(frame);
+    //Ici aussi la frame et bien former
     return (res->cmd == C_EXIT);
 }
 
@@ -143,6 +154,8 @@ bool decodeAcquittalFrame_itShouldThrowsENOTDDPError() {
     //permet de tester pour une taille non détérminer supérieur à 4
     unsigned char frame[] = {A_LOOKUP, 'n', 'o', 't', ' ', 'a', ' ', 'f', 'r', 'a', 'm'};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //La trame envoyer commence par une commande mais la suite n'existe pas donc ce n'est pas une trame du
+    //protocole DDP on renvoie l'erreur ENOTDDP
     return (DDP_Errno == ENOTDDP) && (res->cmd == 0xff);
 }
 
@@ -151,6 +164,8 @@ bool decodeAcquittalFrame_itShouldThrowsENOTDDPError_2() {
     //Permet de tester pour une taille de 4 pille
     unsigned char frame[] = {A_SET, 'n', 'o', 't'};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //La trame envoyer commence par une commande mais la suite n'existe pas donc ce n'est pas une trame du
+    //protocole DDP on renvoie l'erreur ENOTDDP
     return (DDP_Errno == ENOTDDP) && (res->cmd == 0xff);
 }
 
@@ -159,14 +174,16 @@ bool decodeAcquittalFrame_itShouldThrowsENOTDDPError_3() {
     //Permet de tester pour une taille inférieur à 4
     unsigned char frame[] = {A_SET, 'n', 'o'};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //La trame envoyer commence par une commande mais la suite n'existe pas donc ce n'est pas une trame du
+    //protocole DDP on renvoie l'erreur ENOTDDP
     return (DDP_Errno == ENOTDDP) && (res->cmd == 0xff);
 }
 
 bool decodeAcquittalFrame_itShouldThrowsENOTDDPError_4() {
     DDP_Errno = -1;
-    //Une frame avec un drapeau inconue n'est pas du ddp protocole
     unsigned char frame[] = {A_LOOKUP, 1, 0x55, END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //La trame utilise un drapeau inconue par le protocole DDP on renvoie l'erreur ENOTDDP
     return (DDP_Errno == ENOTDDP) && (res->cmd == 0xff);
 }
 
@@ -174,6 +191,8 @@ bool decodeAcquittalFrame_itShouldThrowENOTAQIT() {
     DDP_Errno = -1;
     unsigned char frame[] = {C_SET, 250, SUCCESS, END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //La trame envoyer et correct cependant la commande C_SET n'est pas une commande de type ACQUITTAL
+    //il faut donc renvoyer l'erreur ENOTAQIT
     return (DDP_Errno == ENOTAQIT) && (res->cmd == 0xff);
 }
 
@@ -181,6 +200,8 @@ bool decodeAcquittalFrame_itShouldThrowEWRONGNFLAG_ASET() {
     DDP_Errno = -1;
     unsigned char frame[] = {A_SET, 1, NOT_FOUND, END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Le flag d'erreur not found n'est pas possible pour la trame A_SET
+    //Il faut donc renvoyer WRONG NOT FOUND FLAG
     return (DDP_Errno == EWRONGNFLAG) && (res->cmd == 0xff);
 }
 
@@ -188,27 +209,34 @@ bool decodeAcquittalFrame_itShouldThrowEWRONGNFLAG_ADUMP() {
     DDP_Errno = -1;
     unsigned char frame[] = {A_DUMP, 1, NOT_FOUND, END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Le flag d'erreur not found n'est pas possible pour la trame A_DUMP
+    //Il faut donc renvoyer WRONG NOT FOUND FLAG
     return (DDP_Errno == EWRONGNFLAG) && (res->cmd == 0xff);
 }
 
 bool decodeAcquittalFrame_itShouldThrowEWRONGSFLAG_ALOOKUP() {
     DDP_Errno = -1;
-    unsigned char frame[] = {A_LOOKUP, 1, SUCCESS, END_FRAME};
+    unsigned char frame[] = {A_LOOKUP, 1, INTERNAL_ERROR, 0x0+1,0x2+1,'h','i', END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
-    return (DDP_Errno == EWRONGSFLAG) && (res->cmd == 0xff);
+    //Le flag d'erreur INTERNAL_ERROR ou NOT_FOUND n'est pas possible pour la trame A_LOOKUP contenant des données
+    //Il faut donc renvoyer WRONG ERROR FLAG
+    return (DDP_Errno == EWRONGEFLAG) && (res->cmd == 0xff);
 }
 
-bool decodeAcquittalFrame_itShouldThrowENOENDFLAG() {
+bool decodeAcquittalFrame_itShouldThrowEWRONGEFLAG_ALOOKUP() {
     DDP_Errno = -1;
-    unsigned char frame[] = {A_LOOKUP, 1, INTERNAL_ERROR, 0x55};
+    unsigned char frame[] = {A_LOOKUP, 1, SUCCESS, END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
-    return (DDP_Errno == ENOENDFLAG) && (res->cmd == 0xff);
+    //Le flag de success n'est pas autoriser si aucune donné lui sont associer pour un A_LOOKUP
+    //Il faut donc renvoyer WRONG SUCCESS FLAG
+    return (DDP_Errno == EWRONGSFLAG) && (res->cmd == 0xff);
 }
 
 bool decodeAcquittalFrame_itShouldCorrectlyDecodeThisFrame() {
     DDP_Errno = -1;
     unsigned char frame[] = {A_LOOKUP, 0 + 1, INTERNAL_ERROR, END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Ici la trame et bien composer tous devrais fonctioner
     return (
                    (
                            (res->cmd == A_LOOKUP) &&
@@ -225,6 +253,8 @@ bool decodeAcquittalFrame_itShouldThrowEBADCMD(){
     DDP_Errno = -1;
     unsigned char frame[] = {A_DUMP, 0 + 1, SUCCESS, 0x0+1,0x2+1,'h','i',END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Un A_DUMP ne peut pas renvoyer de donnée c'est un mauvais choix de donné
+    //Il faut renvoyer une erreur de BAD COMMANDE
     return (DDP_Errno == EBADCMD) && (res->cmd == 0xff);
 }
 
@@ -232,6 +262,7 @@ bool decodeAcquittalFrame_itShouldThrowENOTDDPError_4(){
     DDP_Errno = -1;
     unsigned char frame[] = {A_LOOKUP, 0 + 1, 0x55, 0x0+1,0x2+1,'h','i',END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Le flag 0x55 n'existe pas dans DDP il faut donc renvoyer l'erreur NOT DDP
     return (DDP_Errno == ENOTDDP) && (res->cmd == 0xff);
 }
 
@@ -239,6 +270,8 @@ bool decodeAcquittalFrame_itShouldThrowEWRONGEFLAG(){
     DDP_Errno = -1;
     unsigned char frame[] = {A_LOOKUP, 0 + 1, NOT_FOUND, 0x0+1,0x2+1,'h','i',END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Ici la derniere valeur du tableau devrais être END_FRAME or ici ce n'est que 0x55
+    //On s'attend donc à recevoir l'erreur NO END FLAG
     return (DDP_Errno == EWRONGEFLAG) && (res->cmd == 0xff);
 }
 
@@ -246,6 +279,8 @@ bool decodeAcquittalFrame_itShouldThrowENOENDFLAG_2(){
         DDP_Errno = -1;
         unsigned char frame[] = {A_LOOKUP, 0 + 1, SUCCESS, 0x0+1,0x2+1,'h','i',0xEE};
         PAcquittalFrame res = decodeAcquittalFrame(frame);
+        //Ici la derniere valeur du tableau devrais être END_FRAME or ici ce n'est que 0xEE
+        //On s'attend donc à recevoir l'erreur NO END FLAG
         return (DDP_Errno == ENOENDFLAG) && (res->cmd == 0xff);
 }
 
@@ -253,6 +288,8 @@ bool decodeAcquittalFrame_itShouldThrowEWRONGLEN(){
     DDP_Errno = -1;
     unsigned char frame[] = {A_LOOKUP, 0 + 1, SUCCESS, 0x0 + 1,0x3 + 1,'h','i',END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Ici la taille indiquer par la partie data length et de 3 or la longeurs du tableau de donnée ici est de 2
+    //Il faut donc renvoyer une erreur de WRONG LENGTH
     return (DDP_Errno == EWRONGLEN) && (res->cmd == 0xff);
 }
 
@@ -260,6 +297,7 @@ bool decodeAcquittalFrame_itShouldCorrectlyDecodeThisFrame_2(){
     DDP_Errno = -1;
     unsigned char frame[] = {A_LOOKUP, 0 + 1, SUCCESS, 0x0 + 1,0x2 + 1,'h','i',END_FRAME};
     PAcquittalFrame res = decodeAcquittalFrame(frame);
+    //Ici la trame et bien composer tous devrais fonctioner
     return (
                    (
                            (res->cmd == A_LOOKUP) &&
@@ -284,6 +322,8 @@ bool encodeAskFrame_itShouldThrowEBADCMD(){
     askFrame->val=2;
     unsigned char * data = encodeAskFrame(askFrame);
     free(askFrame);
+    //A_SET n'est pas une commande destiner à être utiliser par la trame ASK
+    //Il faut renvoyer une erreur de BAD COMMANDE
     return (DDP_Errno == EBADCMD) && (data[0] == 0xff);
 }
 
@@ -294,12 +334,15 @@ bool encodeAskFrame_itShouldThrowENOTDDP(){
     askFrame->val=2;
     unsigned char * data = encodeAskFrame(askFrame);
     free(askFrame);
+    //0x51 n'est pas une commande de DDP
+    //Il faut renvoyer une erreur de NOT DDP
     return (DDP_Errno == ENOTDDP) && (data[0] == 0xff);
 }
 
 bool encodeAskFrame_itShouldEncodeThisFrame(){
     //25 = 0b 0000 0000 0001 1001
-    //                     0x19
+    //                   0x1  0x9
+    //          0x00       0x19
     unsigned char expectedFrame[] = {C_LOOKUP, 0x0 + 1, 0x19 + 1, END_FRAME};
     DDP_Errno = -1;
     PAskFrame askFrame = (PAskFrame) malloc(sizeof(AskFrame));
@@ -307,6 +350,7 @@ bool encodeAskFrame_itShouldEncodeThisFrame(){
     askFrame->val=25;
     unsigned char * data = encodeAskFrame(askFrame);
     free(askFrame);
+    //ici la trame et bien former on s'attend donc à ce que sa correspondre à la expectedFrame
     return memcmp(expectedFrame,data,4)==0;
 }
 
@@ -317,9 +361,13 @@ bool encodeAskFrame_itShouldEncodeThisFrame_2(){
     askFrame->cmd=C_EXIT;
     unsigned char * data = encodeAskFrame(askFrame);
     free(askFrame);
+    //ici la trame et bien former on s'attend donc à ce que sa correspondre à la expectedFrame
     return memcmp(expectedFrame,data,2)==0;
 }
 
+/************************************
+ *  test de encodeAcquittalFrame()  *
+ ************************************/
 bool encodeAcquittalFrame_itShouldThrowEBADCMD(){
     DDP_Errno = -1;
     PAcquittalFrame acquittalFrame = (PAcquittalFrame) malloc(sizeof(PAcquittalFrame));
@@ -329,6 +377,8 @@ bool encodeAcquittalFrame_itShouldThrowEBADCMD(){
     acquittalFrame->dataLength=0;
     unsigned char * data = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //C_SET n'est pas une commande destiner à être utilisé par la trame ACQUITTAL
+    //Il faut renvoyer une erreur de BAD COMMANDE
     return (DDP_Errno == EBADCMD) && (data[0] == 0xff);
 }
 
@@ -346,6 +396,8 @@ bool encodeAcquittalFrame_itShouldThrowEBADCMD_2(){
     }
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //A_SET ne peut pas contenir de données
+    //Il faut renvoyer une erreur de BAD COMMANDE
     return (DDP_Errno == EBADCMD) && (frame[0] == 0xff);
 }
 
@@ -363,6 +415,8 @@ bool encodeAcquittalFrame_itShouldThrowENOTDDP(){
     }
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //Le flag 0x55 n'est pas un drapeau connu du protocole DDP
+    //Il faut renvoyer une erreur de NOT DDP
     return (DDP_Errno == ENOTDDP) && (frame[0] == 0xff);
 }
 
@@ -375,6 +429,8 @@ bool encodeAcquittalFrame_itShouldThrowENOTDDP_2(){
     acquittalFrame->dataLength=0;
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //Le flag 0x55 n'est pas un drapeau connu du protocole DDP
+    //Il faut renvoyer une erreur de NOT DDP
     return (DDP_Errno == ENOTDDP) && (frame[0] == 0xff);
 }
 
@@ -392,6 +448,8 @@ bool encodeAcquittalFrame_itShouldThrowEWRONGEFLAG(){
     }
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //Le flag d'erreur INTERNAL_ERROR ou NOT_FOUND n'est pas possible pour la commande A_LOOKUP contenant des données
+    //Il faut donc renvoyer WRONG ERROR FLAG
     return (DDP_Errno == EWRONGEFLAG) && (frame[0] == 0xff);
 }
 
@@ -404,6 +462,8 @@ bool encodeAcquittalFrame_itShouldThrowEWRONGNFLAG_A_SET(){
     acquittalFrame->dataLength=0;
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //Le flag d'erreur NOT_FOUND n'est pas possible pour la commande A_SET
+    //Il faut donc renvoyer WRONG NOT FOUND FLAG
     return (DDP_Errno == EWRONGNFLAG) && (frame[0] == 0xff);
 }
 
@@ -416,6 +476,8 @@ bool encodeAcquittalFrame_itShouldThrowEWRONGNFLAG_A_DUMP(){
     acquittalFrame->dataLength=0;
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //Le flag d'erreur NOT_FOUND n'est pas possible pour la commande A_DUMP
+    //Il faut donc renvoyer WRONG NOT FOUND FLAG
     return (DDP_Errno == EWRONGNFLAG) && (frame[0] == 0xff);
 }
 
@@ -428,6 +490,8 @@ bool encodeAcquittalFrame_itShouldThrowEWRONGSFLAG_A_LOOKUP(){
     acquittalFrame->dataLength=0;
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //Le flag d'erreur SUCCESS n'est pas possible pour la commande A_LOOKUP lorsqu'elle ne contient pas de données
+    //Il faut donc renvoyer WRONG SUCCESS FLAG
     return (DDP_Errno == EWRONGSFLAG) && (frame[0] == 0xff);
 }
 
@@ -445,6 +509,8 @@ bool encodeAcquittalFrame_itShouldThrowEWRONGLEN(){
     }
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //Ici la taille indiquer par la partie data length et de 9 or la longeurs du tableau de donnée ici est de 10
+    //Il faut donc renvoyer une erreur de WRONG LENGTH
     return (DDP_Errno == EWRONGLEN) && (frame[0] == 0xff);
 }
 
@@ -465,6 +531,7 @@ bool encodeAcquittalFrame_itShouldEncodeThisFrame(){
     }
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //La frame et correctement formé et devrais être egale à expectedFrame
     return memcmp(expectedFrame,frame,15)==0;
 }
 
@@ -478,6 +545,7 @@ bool encodeAcquittalFrame_itShouldEncodeThisFrame_2(){
     acquittalFrame->dataLength=0;
     unsigned char * frame = encodeAcquittalFrame(acquittalFrame);
     free(acquittalFrame);
+    //La frame et correctement formé et devrais être egale à expectedFrame
     return memcmp(expectedFrame,frame,4)==0;
 }
 
@@ -509,7 +577,7 @@ int main() {
     passTest(daqf, "It should throw an EWRONGNFLAG with A_SET cmd",decodeAcquittalFrame_itShouldThrowEWRONGNFLAG_ASET());
     passTest(daqf, "It should throw an EWRONGNFLAG with A_DUMP cmd",decodeAcquittalFrame_itShouldThrowEWRONGNFLAG_ADUMP());
     passTest(daqf, "It should throw an EWRONGSFLAG with A_LOOKUP cmd",decodeAcquittalFrame_itShouldThrowEWRONGSFLAG_ALOOKUP());
-    passTest(daqf, "It should throw an ENOENDFLAG error", decodeAcquittalFrame_itShouldThrowENOENDFLAG());
+    passTest(daqf, "It should throw an ENOENDFLAG error", decodeAcquittalFrame_itShouldThrowENOENDFLAG_2());
     passTest(daqf, "It should correctly decode this frame", decodeAcquittalFrame_itShouldCorrectlyDecodeThisFrame());
     passTest(daqf, "It should throw an EBADCMD error", decodeAcquittalFrame_itShouldThrowEBADCMD());
     passTest(daqf, "It should throw ENOTDDP error cause of unknown flag",decodeAcquittalFrame_itShouldThrowENOTDDPError_4());
