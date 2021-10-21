5cf8d0442b02e02eef4fac475832319dabfff276
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-10-21 18:10:42 +0200

    Doc : commentaire sur le main

--- process_dictionary/main.c
+++ process_dictionary/main.c
@@ -8,21 +8,23 @@
 
 int main(int argc, char *argv[]){
     int nbNodes;
-
+    //verification qu'il y a bien un argument
     if (argc != 2) {
         fprintf(stderr, "Argument insufisant\n");
         exit(-1);
     }
     nbNodes = atoi(argv[1]);
+    //Verification que l'argument envoyer est un nombre
     if (nbNodes == -1) {
         fprintf(stderr, "Ce n'est pas un nb\n");
         exit(-1);
     }
-
+    //Verification que le nombre de nœuds est supérieur à 2
     if(nbNodes < 2){
         fprintf(stderr, "Au moins 2 processus son requis\n");
         exit(-1);
     }
+    //Lancement du programme
     runController(nbNodes);
     return 0;
 }
